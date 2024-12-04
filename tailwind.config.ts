16a38fe92632559ca8fab693c6f8921cbc7097f7
muzi5@kakao.com <muzi5@kakao.com>
2024-12-04 12:01:54 +0900

    테일윈드 폰트사이즈 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,108 @@ const fontSize: FontSize = {
 			fontWeight: "600", // semiBold
 		},
 	],
+	// Body Large Text
+	bodyLarge400: [
+		"1.125rem",
+		{
+			lineHeight: "1.625rem",
+			fontWeight: "400",
+		},
+	],
+	bodyLarge500: [
+		"1.125rem",
+		{
+			lineHeight: "1.625rem",
+			fontWeight: "500",
+		},
+	],
+	bodyLarge600: [
+		"1.125rem",
+		{
+			lineHeight: "1.625rem",
+			fontWeight: "600",
+		},
+	],
+	bodyLarge700: [
+		"1.125rem",
+		{
+			lineHeight: "1.625rem",
+			fontWeight: "700",
+		},
+	],
+	// Body Medium Text
+	bodyMedium400: [
+		"1rem",
+		{
+			lineHeight: "1.5rem",
+			fontWeight: "400",
+		},
+	],
+	bodyMedium500: [
+		"1rem",
+		{
+			lineHeight: "1.5rem",
+			fontWeight: "500",
+		},
+	],
+	bodyMedium600: [
+		"1rem",
+		{
+			lineHeight: "1.5rem",
+			fontWeight: "600",
+		},
+	],
+	bodyMedium700: [
+		"1rem",
+		{
+			lineHeight: "1.5rem",
+			fontWeight: "700",
+		},
+	],
+	// Body Small Text
+	bodySmall400: [
+		"0.875rem",
+		{
+			lineHeight: "1.375rem",
+			fontWeight: "400",
+		},
+	],
+	bodySmall500: [
+		"0.875rem",
+		{
+			lineHeight: "1.375rem",
+			fontWeight: "500",
+		},
+	],
+	bodySmall600: [
+		"0.875rem",
+		{
+			lineHeight: "1.375rem",
+			fontWeight: "600",
+		},
+	],
+	bodySmall700: [
+		"0.875rem",
+		{
+			lineHeight: "1.375rem",
+			fontWeight: "700",
+		},
+	],
+	// Body Extra Small Regular
+	bodyExtraSmall400: [
+		"0.75rem",
+		{
+			lineHeight: "1.25rem",
+			fontWeight: "400",
+		},
+	],
+	bodyExtraSmall500: [
+		"0.75rem",
+		{
+			lineHeight: "1.25rem",
+			fontWeight: "500",
+		},
+	],
 };
 
 export default {
