8774cb7f6550fc36676a7ebad0197f073eba951e
muzi5@kakao.com <muzi5@kakao.com>
2024-12-14 03:21:23 +0900

    feat: 네비게이션 아이템 컴포넌트 추가 및 아이콘 통합
    NavItem 컴포넌트 추가

--- src/app/_components/header/NavItem.tsx
+++ src/app/_components/header/NavItem.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 import React from "react";
 
 interface NavItemProps {
+	icon: React.ReactNode;
 	children: React.ReactNode;
 	href: string;
 }
-export default function NavItem({ children, href }: NavItemProps) {
-	return <Link href={href}>{children}</Link>;
+export default function NavItem({ icon, children, href }: NavItemProps) {
+	return (
+		<Link className="flex items-center" href={href}>
+			<span className="mr-3">{icon}</span>
+			<span className="text-bodyMedium400 text-secondary">{children}</span>
+		</Link>
+	);
 }
--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,9 +1,16 @@
+import { icons } from "@/shared/libs/common/icons";
+
 import HeaderLogo from "./HeaderLogo";
+import NavItem from "./NavItem";
 
 export default function Header() {
 	return (
 		<header>
 			<HeaderLogo isActive={true} />
+			<br />
+			<NavItem href="/home" icon={icons.home}>
+				home
+			</NavItem>
 		</header>
 	);
 }
