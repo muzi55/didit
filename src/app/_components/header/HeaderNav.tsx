1fb27c775c3d646abeaa4bdbdf085ccf7065ebac
muzi5@kakao.com <muzi5@kakao.com>
2024-12-14 04:20:10 +0900

    feat: HeaderNav 컴포넌트 추가 및 NavItem동적 렌더링 구현

--- src/app/_components/header/HeaderNav.tsx
+++ src/app/_components/header/HeaderNav.tsx
@@ -1,10 +1,71 @@
+"use client";
+import { useSelectedLayoutSegment } from "next/navigation";
 import React from "react";
 
+import { icons } from "@/shared/libs/common/icons";
+
+import NavItem from "./NavItem";
+
+const navArray = [
+	{
+		href: "/peed",
+		segment: "peed",
+		icon: icons.home,
+		activeIcon: icons.homeActive,
+		content: "피드",
+	},
+	{
+		href: "/space",
+		segment: "space",
+		icon: icons.layers,
+		activeIcon: icons.layersActive,
+		content: "스페이스",
+	},
+	{
+		href: "/calendar",
+		segment: "calendar",
+		icon: icons.calendar,
+		activeIcon: icons.calendarActive,
+		content: "캘린더",
+	},
+	{
+		href: "/resume",
+		segment: "resume",
+		icon: icons.comment,
+		activeIcon: icons.commentActive,
+		content: "이력서",
+	},
+	{
+		href: "/search",
+		segment: "search",
+		icon: icons.search,
+		activeIcon: icons.searchActive,
+		content: "검색",
+	},
+	{
+		href: "/notification",
+		segment: "notification",
+		icon: icons.bell,
+		activeIcon: icons.bellActive,
+		content: "알림",
+	},
+];
+
 function HeaderNav() {
+	const segment = useSelectedLayoutSegment();
 	return (
 		<nav>
 			<ul>
-				<li>home</li>
+				{navArray.map(item => (
+					<NavItem
+						key={item.href}
+						href={item.href}
+						active={segment === item.segment}
+						icon={segment === item.segment ? item.activeIcon : item.icon}
+					>
+						피드
+					</NavItem>
+				))}
 			</ul>
 		</nav>
 	);
--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,30 +1,11 @@
-"use client";
-import { useSelectedLayoutSegment } from "next/navigation";
-
-import { icons } from "@/shared/libs/common/icons";
-
 import HeaderLogo from "./HeaderLogo";
-import NavItem from "./NavItem";
+import HeaderNav from "./HeaderNav";
 
 export default function Header() {
-	const segment = useSelectedLayoutSegment();
-
-	console.log(segment);
-
 	return (
 		<header>
 			<HeaderLogo isActive={true} />
-			<br />
-			<NavItem
-				href="/peed"
-				active={segment === "peed"}
-				icon={segment === "peed" ? icons.homeActive : icons.home}
-			>
-				피드
-			</NavItem>
-			<NavItem href="/space" active={segment === "space"} icon={icons.home}>
-				스페이스
-			</NavItem>
+			<HeaderNav />
 		</header>
 	);
 }
