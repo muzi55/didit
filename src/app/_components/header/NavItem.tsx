74515babafccc12268bae3958185150d8816992a
muzi5@kakao.com <muzi5@kakao.com>
2024-12-14 04:07:21 +0900

    feat: 네비게이션 아이템 활성화 상태 추가 및 스타일링

--- src/app/_components/header/NavItem.tsx
+++ src/app/_components/header/NavItem.tsx
@@ -1,16 +1,31 @@
 import Link from "next/link";
 import React from "react";
 
+import style from "./NavItem.module.css";
 interface NavItemProps {
 	icon: React.ReactNode;
 	children: React.ReactNode;
 	href: string;
+	active: boolean;
 }
-export default function NavItem({ icon, children, href }: NavItemProps) {
+export default function NavItem({
+	icon,
+	children,
+	href,
+	active,
+}: NavItemProps) {
+	const navActivePoint = active ? style.navItem : "";
 	return (
-		<Link className="flex items-center" href={href}>
-			<span className="mr-3">{icon}</span>
-			<span className="text-bodyMedium400 text-secondary">{children}</span>
+		<Link
+			className={`relative flex items-center ${navActivePoint}`}
+			href={href}
+		>
+			<span className="ml-6 mr-3">{icon}</span>
+			<span
+				className={`text-bodyMedium400 ${active ? "text-primary" : "text-secondary"}`}
+			>
+				{children}
+			</span>
 		</Link>
 	);
 }
--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,15 +1,29 @@
+"use client";
+import { useSelectedLayoutSegment } from "next/navigation";
+
 import { icons } from "@/shared/libs/common/icons";
 
 import HeaderLogo from "./HeaderLogo";
 import NavItem from "./NavItem";
 
 export default function Header() {
+	const segment = useSelectedLayoutSegment();
+
+	console.log(segment);
+
 	return (
 		<header>
 			<HeaderLogo isActive={true} />
 			<br />
-			<NavItem href="/home" icon={icons.home}>
-				home
+			<NavItem
+				href="/peed"
+				active={segment === "peed"}
+				icon={segment === "peed" ? icons.homeActive : icons.home}
+			>
+				피드
+			</NavItem>
+			<NavItem href="/space" active={segment === "space"} icon={icons.home}>
+				스페이스
 			</NavItem>
 		</header>
 	);
