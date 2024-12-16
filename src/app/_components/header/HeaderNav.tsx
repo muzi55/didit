da08acb15687ea69c64251ace8f5995f60768e9d
muzi5@kakao.com <muzi5@kakao.com>
2024-12-16 14:44:44 +0900

    feat: HeaderNav 드랍다운 기능 추가
    드랍다운 기능 추가

--- src/app/_components/header/HeaderNav.tsx
+++ src/app/_components/header/HeaderNav.tsx
@@ -4,6 +4,8 @@ import React from "react";
 
 import { icons } from "@/shared/libs/common/icons";
 
+import Dropdown from "../dropdown/Dropdown";
+import NavIconWithText from "./NavIconWithText";
 import NavItem from "./NavItem";
 
 const navArray = [
@@ -20,6 +22,11 @@ const navArray = [
 		icon: icons.layers,
 		activeIcon: icons.layersActive,
 		content: "스페이스",
+		dropdown: true,
+		dropdownList: [
+			{ title: "Didit", href: "/#" },
+			{ title: "데일리", href: "/#" },
+		],
 	},
 	{
 		href: "/calendar",
@@ -56,16 +63,34 @@ function HeaderNav() {
 	return (
 		<nav>
 			<ul>
-				{navArray.map(item => (
-					<NavItem
-						key={item.href}
-						href={item.href}
-						active={segment === item.segment}
-						icon={segment === item.segment ? item.activeIcon : item.icon}
-					>
-						피드
-					</NavItem>
-				))}
+				{navArray.map(item => {
+					if (item.dropdown)
+						return (
+							<Dropdown
+								key={item.href}
+								trigger={
+									<NavIconWithText
+										icon={item.icon}
+										active={segment === item.segment}
+									>
+										{item.content}
+									</NavIconWithText>
+								}
+							>
+								<p>item</p>
+							</Dropdown>
+						);
+					return (
+						<NavItem
+							key={item.href}
+							href={item.href}
+							active={segment === item.segment}
+							icon={segment === item.segment ? item.activeIcon : item.icon}
+						>
+							피드
+						</NavItem>
+					);
+				})}
 			</ul>
 		</nav>
 	);
