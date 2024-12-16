08bf54655c25af16fdf7bb1a8fcf629c579f128b
muzi5@kakao.com <muzi5@kakao.com>
2024-12-17 01:12:37 +0900

    style: 대체이미지 추가

--- src/app/_components/header/profile/HeaderProfile.tsx
+++ src/app/_components/header/profile/HeaderProfile.tsx
@@ -4,6 +4,7 @@ import React from "react";
 export default function HeaderProfile() {
 	const 신세경사진 =
 		"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Shin_Se-kyung_%EC%8B%A0%EC%84%B8%EA%B2%BD_in_January_2024_03.jpg/800px-Shin_Se-kyung_%EC%8B%A0%EC%84%B8%EA%B2%BD_in_January_2024_03.jpg";
+
 	return (
 		<div className="flex items-center bg-red-300 ml-[16px]">
 			<div className="mr-[8px]">
