833abb3a3478587ea40beb2aed8e378eef5a5c2e
muzi5@kakao.com <muzi5@kakao.com>
2024-12-17 12:40:27 +0900

    feat: 사이드바 도글 기능 추가 및 아이콘 컴포넌트 추가

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,13 +1,23 @@
+"use client";
+import { icons } from "@/shared/libs/common/icons";
+import { useHeaderSlide } from "@/shared/store/header/useHeaderSlide";
+
 import HeaderLogo from "./HeaderLogo";
 import HeaderNav from "./HeaderNav";
 import HeaderProfile from "./profile/HeaderProfile";
 
 export default function Header() {
+	const { isSidebarOpen, setToggleSidebar } = useHeaderSlide();
+	const sidebarRotate = isSidebarOpen ? "rotate-180" : "";
+
 	return (
 		<header>
 			<HeaderLogo isActive={true} />
 			<HeaderNav />
 			<HeaderProfile />
+			<button onClick={setToggleSidebar}>
+				<div className={sidebarRotate}>{icons.doubleAngle}</div>
+			</button>
 		</header>
 	);
 }
