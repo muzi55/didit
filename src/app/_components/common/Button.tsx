80481da7e95e6e055d8cc5229f0da243b7dfee62
muzi5@kakao.com <muzi5@kakao.com>
2025-01-11 15:55:46 +0900

    component: 버튼 컴포넌트 추가
    버튼 컴포넌트 추가

--- src/app/_components/common/Button.tsx
+++ src/app/_components/common/Button.tsx
@@ -1,16 +1,43 @@
+"use client";
 import React from "react";
 
 interface ButtonProps {
 	children: React.ReactNode;
 	full?: boolean;
-	color: "primary" | "secondary";
-	size?: "small" | "medium" | "large";
+	color: "primary" | "secondary" | "white";
+	size?: "medium";
+	round?: boolean;
+	onClick: () => void;
 }
 export default function Button({
 	children = "button",
 	full,
 	color = "primary",
 	size = "medium",
+	round = false,
+	onClick,
 }: ButtonProps) {
-	return <button className={`$`}>{children}</button>;
+	const sizeClass = {
+		medium: "px-[28px] py-[9px]",
+	};
+
+	const colorClass = {
+		primary: "bg-primary border-primary",
+		secondary: "bg-secondary text-white border-secondary",
+		white: "bg-white text-black text-black border-black border",
+	};
+
+	const widthFull = full ? "w-full" : "";
+
+	const rounded = round ? "rounded-full" : "rounded-[6px]";
+
+	return (
+		<button
+			className={`${widthFull} border ${sizeClass[size]} ${colorClass[color]} ${rounded}`}
+			onClick={onClick}
+			{...rest}
+		>
+			{children}
+		</button>
+	);
 }
