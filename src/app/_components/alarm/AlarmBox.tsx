a2ccc8caf614bdc1dd10d7f604ae45f932e0f3a0
muzi5@kakao.com <muzi5@kakao.com>
2025-01-21 05:34:05 +0900

    component: MyAlarmSection 컴포넌트 추가 및 하위 컴포넌트 추가
    Header - 해당 섹션의 제목을 나타냅니다.
    Content - 알림의 배열입니다

--- src/app/_components/alarm/AlarmBox.tsx
+++ src/app/_components/alarm/AlarmBox.tsx
@@ -4,6 +4,32 @@ import React, { useState } from "react";
 import { crossMedium, icons } from "@/shared/libs/common/icons";
 
 import { AlarmTabs } from "./AlarmTabs";
+import MyAlarmSection from "./MyAlarmSection";
+
+const textArr = [
+	{
+		projectTitle: "디딧",
+		content: "디딧 프로젝트내용",
+		time: "5분",
+		img: "",
+		onClick: () => {},
+	},
+	{
+		projectTitle: "디딧1",
+		content:
+			"디딧 프로젝트내용디딧프로젝트내용디딧 프로젝트내용디딧 프로젝트내용",
+		time: "12분",
+		img: "",
+		onClick: () => {},
+	},
+	{
+		projectTitle: "디딧55",
+		content: "디딧 프로젝디딧 프로젝트내용트내용",
+		time: "5분",
+		img: "",
+		onClick: () => {},
+	},
+];
 
 export default function AlarmBox() {
 	const [tab, setTab] = useState<number>(0);
@@ -13,7 +39,7 @@ export default function AlarmBox() {
 
 			<AlarmTabs activeIndex={tab} setActiveIndex={setTab}>
 				<AlarmTabs.Tab title="나에게 온 알림">
-					<div>알림1</div>
+					<MyAlarmSection alarmList={textArr} sectionTitle="나에게 온 알림" />
 				</AlarmTabs.Tab>
 				<AlarmTabs.Tab title="알림 설정">
 					<div>알림2</div>
@@ -26,7 +52,7 @@ export default function AlarmBox() {
 const AlarmHeader = ({ onClick }: { onClick: () => void }) => {
 	return (
 		<div className="flex justify-between py-[30px] border-b border-stroke px-[20px]">
-			<h4 className="text-H6">알림</h4>
+			<h2 className="text-H6">알림</h2>
 			<button onClick={onClick}>{icons.cross(crossMedium)}</button>
 		</div>
 	);
---
+++ src/app/_components/alarm/MyAlarmSection.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import React from "react";
+
+import { AlarmImg } from "./AlarmImage";
+
+interface MyAlarmSectionProps {
+	sectionTitle: string;
+	alarmList: MyAlarmContentProps[];
+}
+
+export default function MyAlarmSection({
+	sectionTitle,
+	alarmList,
+}: MyAlarmSectionProps) {
+	return (
+		<div className="px-[20px]">
+			<MyAlarmSection.Header>{sectionTitle}</MyAlarmSection.Header>
+			<ul className="flex flex-col gap-[20px]">
+				{alarmList.map(alarm => (
+					<MyAlarmSection.Content
+						key={alarm.projectTitle}
+						projectTitle={alarm.projectTitle}
+						content={alarm.content}
+						onClick={alarm.onClick}
+						time={alarm.time}
+						img={alarm.img}
+					/>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+const MyAlarmHeader = ({ children }: { children: string }) => {
+	return <h3 className="text-bodyMedium700 my-[20px]">{children}</h3>;
+};
+
+interface MyAlarmContentProps {
+	projectTitle: string;
+	content: string;
+	onClick: () => void;
+	time: string;
+	img?: string;
+}
+
+const MyAlarmContent = ({
+	projectTitle,
+	content,
+	onClick,
+	time,
+	img,
+}: MyAlarmContentProps) => {
+	const { AlarmLogo } = AlarmImg;
+	const checkImg = img ? { ...AlarmImg, src: img } : AlarmLogo;
+
+	return (
+		<div className="flex items-center py-[10px]">
+			<div className="mr-[20px]">
+				<Image {...checkImg} alt={AlarmLogo.alt} />
+			</div>
+			<div className="flex-1">
+				<div className="flex justify-between mb-[5px]">
+					<h4 className="text-bodyMedium500 text-textColor-secondary">
+						{projectTitle}
+					</h4>
+					<button onClick={onClick}>x</button>
+				</div>
+				<div className="flex justify-between">
+					<div className="text-bodySmall400 line-clamp-2 mr-[9px]">
+						{content}
+					</div>
+					<p className="self-end whitespace-nowrap text-bodyExtraSmall400 text-textColor-secondary">
+						{time} 전
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+MyAlarmSection.Header = MyAlarmHeader;
+MyAlarmSection.Content = MyAlarmContent;
