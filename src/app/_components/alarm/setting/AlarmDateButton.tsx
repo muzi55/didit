c7659926b9484562d73fb8d07c7943a24fe35247
muzi5@kakao.com <muzi5@kakao.com>
2025-01-21 09:08:56 +0900

    component: AlarmSetting에 날짜 선택 기능 추가 및 선택 상태 관리 로직 구현

--- src/app/_components/alarm/setting/AlarmDateButton.tsx
+++ src/app/_components/alarm/setting/AlarmDateButton.tsx
@@ -3,14 +3,17 @@ import React from "react";
 interface AlarmDateButtonProps {
 	children: React.ReactNode;
 	onClick: () => void;
+	active: boolean;
 }
 export default function AlarmDateButton({
 	children,
 	onClick,
+	active,
 }: AlarmDateButtonProps) {
+	const style = active ? "bg-secondary text-white" : "bg-gray-1 text-dark-6";
 	return (
 		<button
-			className="w-full rounded-lg text-bodySmall400 py-[6px] text-center bg-secondary text-white"
+			className={`w-full rounded-lg text-bodySmall400 py-[6px] text-center ${style}`}
 			onClick={onClick}
 		>
 			{children}
--- src/app/_components/alarm/setting/AlarmSetting.tsx
+++ src/app/_components/alarm/setting/AlarmSetting.tsx
@@ -35,6 +35,33 @@ const AlarmSettingSection = () => {
 		},
 	});
 
+	const handleCheckDate = (day: keyof AlarmCheckDatetime["day"]) => {
+		// TODO refactor
+		setDateCheck({
+			...dateCheck,
+			day: {
+				...dateCheck.day,
+				[day]: !dateCheck.day[day],
+			},
+		});
+	};
+
+	const handleCheckAllDate = () => {
+		const allChecked = Object.values(dateCheck.day).every(value => value);
+		const anyChecked = Object.values(dateCheck.day).some(value => value);
+
+		setDateCheck({
+			...dateCheck,
+			day: {
+				mon: !allChecked && anyChecked ? true : !allChecked,
+				tue: !allChecked && anyChecked ? true : !allChecked,
+				wed: !allChecked && anyChecked ? true : !allChecked,
+				thu: !allChecked && anyChecked ? true : !allChecked,
+				fri: !allChecked && anyChecked ? true : !allChecked,
+			},
+		});
+	};
+
 	return (
 		<section className="flex flex-col gap-[20px] pt-[20px] border-t border-stroke">
 			<div>
@@ -44,13 +71,59 @@ const AlarmSettingSection = () => {
 				</p>
 			</div>
 
-			<AlarmSettingBox title="알림 시간">
-				123
+			<AlarmSettingBox title="날짜 설정">
 				<div>
-					<AlarmDateButton onClick={() => {}}>{"평일 (월-금)"}</AlarmDateButton>
+					<AlarmDateButton
+						active={
+							dateCheck.day.mon &&
+							dateCheck.day.tue &&
+							dateCheck.day.wed &&
+							dateCheck.day.thu &&
+							dateCheck.day.fri
+						}
+						onClick={handleCheckAllDate}
+					>
+						{"평일 (월-금)"}
+					</AlarmDateButton>
+				</div>
+
+				<p className="text-bodySmall400 text-dark-5">날짜별 선택</p>
+
+				<div className="flex gap-2">
+					<AlarmDateButton
+						active={dateCheck.day.mon}
+						onClick={() => handleCheckDate("mon")}
+					>
+						{"월"}
+					</AlarmDateButton>
+					<AlarmDateButton
+						active={dateCheck.day.tue}
+						onClick={() => handleCheckDate("tue")}
+					>
+						{"화"}
+					</AlarmDateButton>
+					<AlarmDateButton
+						active={dateCheck.day.wed}
+						onClick={() => handleCheckDate("wed")}
+					>
+						{"수"}
+					</AlarmDateButton>
+					<AlarmDateButton
+						active={dateCheck.day.thu}
+						onClick={() => handleCheckDate("thu")}
+					>
+						{"목"}
+					</AlarmDateButton>
+					<AlarmDateButton
+						active={dateCheck.day.fri}
+						onClick={() => handleCheckDate("fri")}
+					>
+						{"금"}
+					</AlarmDateButton>
 				</div>
 			</AlarmSettingBox>
-			<AlarmSettingBox title="시간 설정">123</AlarmSettingBox>
+
+			<AlarmSettingBox title="시간 설정">시간 설정</AlarmSettingBox>
 		</section>
 	);
 };
