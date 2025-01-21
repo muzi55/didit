9f61f3a68c7788df14170dadc2df61c14f2be08b
muzi5@kakao.com <muzi5@kakao.com>
2025-01-21 20:43:40 +0900

    component: Search 컴포넌트 구조 작성

---
+++ src/app/_components/search/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Search() {
+	return (
+		<div className="max-w-[854px] w-[80%] min-h-[294px] bg-white">
+			<div>
+				<div>{/* input */}</div>
+				<div>{/* tabs */}</div>
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import Alarm from "./_components/alarm/Alarm";
+import Search from "./_components/search/Search";
 
 export default function Home() {
 	return (
 		<>
 			<div>test</div>
 			<Alarm />
+			<div className="absolute top-0 left-0 w-full h-full bg-[#20202020] z-10 flex justify-center items-center">
+				<Search />
+			</div>
 		</>
 	);
 }
