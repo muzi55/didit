1bdda402f85c9f939cb08a181c5bcc954391976b
muzi5@kakao.com <muzi5@kakao.com>
2025-01-22 00:53:56 +0900

    style: Modal 박스 쉐도우 누락 수정

--- src/app/_components/modal/Modal.tsx
+++ src/app/_components/modal/Modal.tsx
@@ -46,7 +46,7 @@ function ModalContainer({
 			className="fixed top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,0.5)] z-50 flex justify-center items-center"
 			onClick={handleBackgroundClick}
 		>
-			<div className="bg-white rounded-2xl flex overflow-hidden">
+			<div className="bg-white rounded-2xl flex overflow-hidden shadow-shadow3">
 				{children}
 			</div>
 		</div>
