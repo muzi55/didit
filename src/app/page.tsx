632c4e633524c4a0325870b37277577d433414fd
muzi5@kakao.com <muzi5@kakao.com>
2025-01-24 02:36:28 +0900

    component: TitleHeading컴포넌트 추가 및 스토리 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@
 
 import { useState } from "react";
 
-import Alarm from "./_components/alarm/Alarm";
-import Modal from "./_components/modal/Modal";
-import SearchResult from "./_components/search/result/SearchResult";
-import Search from "./_components/search/Search";
+import Space from "./_components/space/Space";
 
 export default function Home() {
 	const [active, setActive] = useState<boolean>(false);
@@ -21,14 +18,16 @@ export default function Home() {
 			<div>test</div>
 
 			<button onClick={openActive}>모달 열기</button>
+			<Space />
 
-			<Alarm />
-			{active && (
+			<hr />
+
+			{/* {active && (
 				<Modal closeModal={closeActive}>
 					<Search />
 				</Modal>
 			)}
-			<SearchResult />
+			<SearchResult /> */}
 			{/* <div className="absolute top-0 left-0 w-full h-full bg-[#20202020] z-10 flex justify-center items-center">
 				
 			</div> */}
