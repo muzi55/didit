b99ccf687103e6b9be9ec8c275d88e066cf41401
muzi5@kakao.com <muzi5@kakao.com>
2025-01-29 05:29:18 +0900

    component: Reminiscence 컴포넌트 추가 및 페이지에 통합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
 "use client";
 
-import { icons } from "@/shared/libs/common/icons";
-
-import DetailTitleHeading from "./_components/common/title/DetailTitleHeading";
-import TitleHeading from "./_components/common/title/TitleHeading";
-import TitleParent from "./_components/common/title/TitleParent";
-import TItleTriggerListItem from "./_components/common/title/TItleTriggerListItem";
-import Space from "./_components/space/Space";
+import Reminiscence from "./_components/reminiscence/Reminiscence";
 
 export default function Home() {
 	// console.log(year, month);
 
 	return (
 		<>
-			<TitleParent>
+			<Reminiscence></Reminiscence>
+			{/* <TitleParent>
 				<TitleHeading>스페이스</TitleHeading>
-			</TitleParent>
+			</TitleParent> */}
 
-			<TitleParent>
+			{/* <TitleParent>
 				<DetailTitleHeading
 					trigger={
 						<>
@@ -40,14 +35,13 @@ export default function Home() {
 				>
 					ㅁㄴㅇ
 				</DetailTitleHeading>
-			</TitleParent>
+			</TitleParent> */}
 
 			{/* <DetailTitleHeading /> */}
-			<Space />
+			{/* <Space /> */}
 
 			{/* <button onClick={openActive}>모달 열기</button> */}
 
-			<hr />
 			{/* {active && (
 				<Modal closeModal={closeActive}>
 					<Search />
